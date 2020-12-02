525ef6556c39aaf5e2e37a3af074bb6c18f28d9e
Anders Jorgensen <virtualzeth@gmail.com>
2020-12-02 15:11:14 +0100

    update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+    public void changeView(ActionEvent e) throws IOException {
+        Parent bubbleSortParent = FXMLLoader.load(getClass().getResource("BubbleSort.fxml"));
+        Scene bubbleSortScene = new Scene(bubbleSortParent);
+
+        Stage window = (Stage) ((Node)e.getSource()).getScene().getWindow();
+        window.setScene(bubbleSortScene);
+        window.show();
+    }
 }
