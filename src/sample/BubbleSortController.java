58aedd7f453008973cf94f2e8e615cf63c2a3e43
virtualzeth <virtualzeth@gmail.com>
2020-12-04 12:33:08 +0100

    Persistence feature

--- src/sample/BubbleSortController.java
+++ src/sample/BubbleSortController.java
@@ -1,94 +1,91 @@
-    package sample;
+package sample;
 
-    import javafx.application.Platform;
-    import javafx.event.ActionEvent;
-    import javafx.fxml.FXML;
-    import javafx.fxml.FXMLLoader;
-    import javafx.fxml.Initializable;
-    import javafx.scene.Node;
-    import javafx.scene.Parent;
-    import javafx.scene.Scene;
-    import javafx.scene.chart.BarChart;
-    import javafx.scene.chart.CategoryAxis;
-    import javafx.scene.chart.NumberAxis;
-    import javafx.scene.chart.XYChart;
-    import javafx.stage.Stage;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
 
-    import java.io.IOException;
-    import java.net.URL;
-    import java.util.ResourceBundle;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    public class BubbleSortController implements Initializable {
-        @FXML
-        private NumberAxis yAxis;
-        @FXML
-        private CategoryAxis xAxis;
-        @FXML
-        private BarChart<String, Number> barChart;
+public class BubbleSortController implements Initializable {
+    @FXML
+    private NumberAxis yAxis;
+    @FXML
+    private CategoryAxis xAxis;
+    @FXML
+    private BarChart<String, Number> barChart;
 
-        private int[] data;
-        private XYChart.Series<String, Number> series = new XYChart.Series<>();
+    private int[] data;
+    private XYChart.Series<String, Number> series = new XYChart.Series<>();
 
-        public void changeScreenToMain(ActionEvent e) throws IOException {
-            Parent bubbleSortParent = FXMLLoader.load(getClass().getResource("Main.fxml"));
-            Scene bubbleSortScene = new Scene(bubbleSortParent);
+    public void changeScreenToMain(ActionEvent e) throws IOException {
+        Parent bubbleSortParent = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene bubbleSortScene = new Scene(bubbleSortParent);
 
-            Stage window = (Stage) ((Node)e.getSource()).getScene().getWindow();
-            window.setScene(bubbleSortScene);
-            window.show();
-        }
-        public void saveFile(ActionEvent e) {
-            System.out.println("Save a file!");
-        }
+        Stage window = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        window.setScene(bubbleSortScene);
+        window.show();
+    }
 
-        public void sort() {
-            new Thread(() -> {
-                int temp = 0;
-                for (int i = 0; i < this.data.length; i++) {
-                    for (int j = 1; j < this.data.length - i; j++) {
-                        if(this.data[j] < this.data[j - 1]) {
-                            temp = this.data[j];
-                            this.data[j] = this.data[j - 1];
-                            this.data[j - 1] = temp;
-                            System.out.println();
-                            this.series = new XYChart.Series<>();
-                            for (int k = 0; k < this.data.length; k++) {
-                                this.series.getData().add(new XYChart.Data<>(Integer.toString(k + 1),this.data[k]));
-                            }
-                            try {
-                                Thread.sleep(200);
-                            } catch(Exception e) {
-                                e.printStackTrace();
-                            }
-                            Platform.runLater(() -> {
-                                barChart.getData().clear();
-                                barChart.getData().add(this.series);
-                            });
+    public void sort() {
+        new Thread(() -> {
+            int temp = 0;
+            for (int i = 0; i < this.data.length; i++) {
+                for (int j = 1; j < this.data.length - i; j++) {
+                    if (this.data[j] < this.data[j - 1]) {
+                        temp = this.data[j];
+                        this.data[j] = this.data[j - 1];
+                        this.data[j - 1] = temp;
+
+                        this.series = new XYChart.Series<>();
+                        for (int k = 0; k < this.data.length; k++) {
+                            this.series.getData().add(new XYChart.Data<>(Integer.toString(k + 1), this.data[k]));
                         }
+                        try {
+                            Thread.sleep(80);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        Platform.runLater(() -> {
+                            barChart.getData().clear();
+                            barChart.getData().add(this.series);
+                        });
                     }
                 }
-            }).start();
-
-        }
-
-        @Override
-        public void initialize(URL url, ResourceBundle rb) {
-            this.data = randomArray(10);
-
-            for (int i = 0; i < this.data.length; i++) {
-                this.series.getData().add(new XYChart.Data<>(Integer.toString(i + 1),this.data[i]));
             }
-            barChart.setAnimated(false);
-            barChart.getData().add(this.series);
-        }
+        }).start();
 
-        public int[] randomArray(int size) {
-            int[] arr = new int[size];
-
-            for (int i = 0; i < size; i++) {
-                arr[i] = (int) (Math.random() * 100) + 1;
-            }
-            return arr;
-        }
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        this.data = randomArray(20);
+
+        for (int i = 0; i < this.data.length; i++) {
+            this.series.getData().add(new XYChart.Data<>(Integer.toString(i + 1), this.data[i]));
+        }
+        barChart.setAnimated(false);
+        barChart.getData().add(this.series);
+    }
+
+    public int[] randomArray(int size) {
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = (int) (Math.random() * 100) + 1;
+        }
+        return arr;
+    }
+}
 
--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -3,15 +3,27 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
-public class MainController {
+public class MainController implements Initializable {
+    @FXML
+    private Label lastVisitLabel;
+
     public void changeScreenToBubbleSort(ActionEvent e) throws IOException {
         Parent bubbleSortParent = FXMLLoader.load(getClass().getResource("BubbleSort.fxml"));
         Scene bubbleSortScene = new Scene(bubbleSortParent);
@@ -23,5 +35,36 @@ public class MainController {
     public void exit(ActionEvent e) {
         Stage window = (Stage) ((Node)e.getSource()).getScene().getWindow();
         window.close();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            if(new File("DateLog.txt").exists()) {
+                BufferedReader reader = new BufferedReader(new FileReader("./DateLog.txt"));
+                String line = reader.readLine();
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+                Date firstDate = sdf.parse(line);
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                LocalDateTime now = LocalDateTime.now();
+                String date = dtf.format(now);
+                Date secondDate = sdf.parse(date);
+                long diffInMillis = Math.abs(secondDate.getTime() - firstDate.getTime());
+                long diff = TimeUnit.DAYS.convert(diffInMillis, TimeUnit.MILLISECONDS);
+                if(diff == 0) {
+                    lastVisitLabel.setText("Your last visit was today!");
+                } else lastVisitLabel.setText(String.format("Your last visit was %s days ago!", diff));
+            }
+
+            try (PrintWriter out = new PrintWriter("DateLog.txt")) {
+                LocalDateTime now = LocalDateTime.now();
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                String date = dtf.format(now);
+                out.println(date);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
